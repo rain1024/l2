38d3442376fb5824aec456fccf21d34cfe7b14b4
Vu Anh <anhv.ict91@gmail.com>
2023-05-14 12:48:17 +0700

    Initial Commit

---
+++ l2/l2App.swift
@@ -0,0 +1,17 @@
+//
+//  l2App.swift
+//  l2
+//
+//  Created by Vu Anh on 14/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct l2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
